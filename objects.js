ac39706e3c4e351680eb9e29c6e30c225182633a
Ernie Val <erndval@gmail.com>
2019-02-23 23:13:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { FalloutBoy: "The Phoenix" };
 
-function updatePlaylist(playlist, artist, song) {
+function updatePlaylist(playlist, artistName, songName) {
   playlist.artist = 'song';
   return playlist
 }
