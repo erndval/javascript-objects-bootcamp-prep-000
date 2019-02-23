e9346c92dc9f166135deb6879034f2ffbe5f108d
Ernie Val <erndval@gmail.com>
2019-02-23 23:24:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artistName, songName) {
   playlist[artistName] = songName;
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
+}
